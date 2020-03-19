02e6d964e2bb778462d79bb8d650c9950761bae1
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-03-19 18:13:06 +0530

    awk commands in linux

---
+++ awk.sh
@@ -0,0 +1,58 @@
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ echo $USER
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ echo $SHELL
+/usr/bin/bash
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ echo $HOME
+/c/Users/HP
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ $mypid
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ $mypid
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ echo Hello Tom
+Hello Tom
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ echo Hello Tom | awk '{ print $0 }'
+Hello Tom
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ echo Hello Tom | awk '{ print $2 }'
+Tom
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ echo Hello Tom | awk '{ $2 = "Adam" } { print $0 }'
+Hello Adam
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ nano -T 3 Environment.sh
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ nano -T 3 awk.sh
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ nano -T 3 awk.sh
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ echo Hello Tom
+Hello Tom
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ echo Hello Tom | awk '{ print $0 }'
+Hello Tom
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ echo Hello Tom | awk '{ print $2 }'
+Tom
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$ echo Hello Tom | awk '{ $2 = "Adam" } { print $0 }'
+Hello Adam
+
+HP@DESKTOP-049SVLJ MINGW64 ~/Desktop/Bridgelabz/Linux commands (master)
+$
+
