01bb5a657aaa9fa6e469ab4ba8579320b96bb134
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-03-19 08:55:15 +0530

    file directory commands

---
+++ file.sh
@@ -0,0 +1,16 @@
+ cd
+  168  cd Desktop/Bridgelabz/Linux\ commands/
+  169  git init
+  170  git remote add origin https://github.com/sanjujaiswal/linux-command.git
+  171  touch file.sh
+  172  pwd
+  173  ls .
+  174  ls ~
+  175  ls -l
+  176  touch test
+  177  ls
+  178  history | git > file.sh
+  179  nano -T 3 file.sh
+  180  history
+
+
